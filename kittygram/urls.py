ef0e06700a3cf6fcc8c279fdc3cf0372758f6083
Anastasia Barshina <barshina.av@gmail.com>
2023-12-12 23:33:21 +0300

    Add ViewSets

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,26 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
 
-from cats.views import cat_list
+from cats.views import CatViewSet  # CatList, CatDetail  # APICat, cat_list
+
+
+# Создаётся роутер
+router = SimpleRouter()
+# Вызываем метод .register с нужными параметрами
+router.register('cats', CatViewSet)
+# В роутере можно зарегистрировать любое количество пар "URL, viewset":
+# например
+# router.register('owners', OwnerViewSet)
+# Но нам это пока не нужно
 
 urlpatterns = [
-   path('cats/', cat_list),
+    # path('cats/', CatList.as_view()),
+    # path('cats/<int:pk>/', CatDetail.as_view()),
+
+    # path('cat/', CatViewSet.as_view(), name='cat-list'),
+    # path('cat/<int:pk>/', CatViewSet.as_view(), name='cat-detail'),
+
+    # Все зарегистрированные в router пути доступны в router.urls
+    # Включим их в головной urls.py
+    path('', include(router.urls)),
 ]
-
-
